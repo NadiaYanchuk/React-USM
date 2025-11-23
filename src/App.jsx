10dd3937132d99343d1863a709139e66d70ae4d7
NadejdaIanciuc <nadiaianciuk@gmail.com>
2025-11-23 15:50:56 +0200

    Add structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="text-6xl font-bold">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
